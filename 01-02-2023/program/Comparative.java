21033b2e64adc615171578eb20397fccd3dfc86f
Deepika <123929463+Deepika-java@users.noreply.github.com>
2023-02-02 18:09:38 +0530

    Add files via upload

---
+++ 01-02-2023/program/Comparative.java
@@ -0,0 +1,21 @@
+class Comparative
+{
+	public static void main(String[]args)
+	{
+		/*
+			==  -> Double equalto
+			!=	-> Not equalto
+			<	-> Lesser than
+			>	-> Greater than
+			<=	-> Lesser than or equalto
+			>=	-> Greater than or equalto
+		*/
+		System.out.println("3 == 4 " + (3 == 4));
+		System.out.println("4 != 4 " + (4 != 4));
+		System.out.println("3 < 4 "  + (3 < 4));
+		System.out.println("3 > 4 "  + (3 > 4));
+		System.out.;println("3 <= 4 " + (3 <= 4));
+		System.out.println("3 >= 4 " + (3 >= 4));
+	}
+
+}

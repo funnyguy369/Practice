3615dbfdb42e0c08d62b74cf0d01210167b3c9e5
Shankar Balaji <72439750+funnyguy369@users.noreply.github.com>
2023-02-04 00:53:58 +0530

    Update Input.java

--- 02-01-2023/program/Input.java
+++ 02-01-2023/program/Input.java
@@ -5,7 +5,7 @@ class Input
 	public static void main(String[]args)
 	{
 		Scanner s = new Scanner(System.in);
-
+		
 		System.out.print("Enter a number: ");
 		int n = s.nextInt();
 		System.out.println("Entered value : " + n);
